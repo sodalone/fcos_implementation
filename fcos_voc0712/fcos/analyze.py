6da896c82869c55c13f82b1e71eadfe04d8ce3aa
sodalone <soda791401863@gmail.com>
2020-06-13 10:14:53 +0800

    fix analyze

--- fcos_voc0712/fcos/analyze.py
+++ fcos_voc0712/fcos/analyze.py
@@ -14,29 +14,29 @@ import matplotlib.pyplot as plt
 
 log = np.load('log.npy', allow_pickle=True)
 
-map_mean, map_50, map_75, loss = [], [], [], []
+ap_mean, ap_50, ap_75, loss = [], [], [], []
 
 for i in range(len(log)):
-    map_mean.append(log[i][0])
-    map_50.append(log[i][1])
-    map_75.append(log[i][2])
+    ap_mean.append(log[i][0])
+    ap_50.append(log[i][1])
+    ap_75.append(log[i][2])
     loss.extend(log[i][3])
 
 fig, (ax1, ax2) = plt.subplots(1, 2)
 
-ax1.plot(map_mean, color='r', label='map_mean')
-ax1.plot(map_50, color='b', label='map_50')
-ax1.plot(map_75, color='g', label='map_75')
+ax1.plot(ap_mean, color='r', label='ap_mean')
+ax1.plot(ap_50, color='b', label='ap_50')
+ax1.plot(ap_75, color='g', label='ap_75')
 ax1.legend()
 ax2.plot(loss, color='black', label='loss')
 ax2.legend()
 
-map_mean = np.around(map_mean, decimals=4)
-map_50 = np.around(map_50, decimals=4)
-map_75 = np.around(map_75, decimals=4)
+ap_mean = np.around(ap_mean, decimals=4)
+ap_50 = np.around(ap_50, decimals=4)
+ap_75 = np.around(ap_75, decimals=4)
 
-print('No', '\t', 'map_mean', '\t', 'map_50', '\t', 'map_75')
-for i in range(len(map_mean)):
-    print('%-9d%-9.4f\t%-9.4f\t%-9.4f' % (i, map_mean[i], map_50[i], map_75[i]))
+print('No', '\t', 'ap_mean', '\t', 'ap_50', '\t\t', 'ap_75')
+for i in range(len(ap_mean)):
+    print('%-9d%-9.4f\t%-9.4f\t%-9.4f' % (i, ap_mean[i], ap_50[i], ap_75[i]))
 
 plt.show()
